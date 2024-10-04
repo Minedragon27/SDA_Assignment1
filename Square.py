d73bcabfc9dfa27d4aea69404bb9f4f13cf119c5
ApprovedByDaan <daanvanalebeek@gmail.com>
2024-10-04 14:52:30 +0200

    Add files via upload

--- Square.py
+++ Square.py
@@ -1,5 +1,6 @@
 import math
 import pygame
+from pygame.locals import *
 from Shape import *
 
 class Square(Shape):
@@ -10,24 +11,28 @@ class Square(Shape):
     def getCenter(self, orientation, sideLength):
         self.__sideLength = sideLength
         self.__orientation = orientation
-        self.width = math.sin(self.__orientation)*self.__sideLength
-        self.height = math.cos(self.__orientation)*self.__sideLength
-        self.X = (self.width/2+self.height/2)
+        self.width = math.sin(self.__orientation)*self.__sideLength + math.cos(self.__orientation)*self.__sideLength
+        self.X = (self.width/2+self.width/2)
         self.Y = self.X
         self.center = [self.X, self.Y]
+        self.rect = pygame.Rect(self.X-(self.width/2), self.Y+(self.width/2), self.__sideLength ,self.__sideLength)
         return self.center
         
     def getShapetype(self):
         return "Square"
 
-    def clickedOn(self, mousePoint):
-        clicked = self.rect.collidepoint(mousePoint)
-        return clicked
-
-    def drawShape(self, window, sideLength):
+    def drawShape(self, window: pygame.display, sideLength, orientation, position):
         self.__sideLength = sideLength
+        self.__orientation = orientation
+        self.__position = position
         scale_Factor = 1
         square_size = self.__sideLength * scale_Factor
         square = pygame.Surface((square_size, square_size), pygame.SRCALPHA)
         square.fill(self.__color)
-        pygame.draw.rect(window, self.__color, (self.__position, self.width, self.height))
+        rotated_surface = pygame.transform.rotate(square, orientation)
+        window.blit(rotated_surface, square)
+
+    def clickedOn(self, mousePoint):
+        mask = pygame.mask.from_surface(square)
+        clicked = self.rect.collidepoint(mousePoint)
+        return clicked
